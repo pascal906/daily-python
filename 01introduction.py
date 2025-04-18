b8ed09da2a8dd738e9d5c4e675ec0cb6e9705a54
Paskal <paskalis007stars@gmail.com>
2025-04-18 13:12:29 +0800

    introduction, python

---
+++ 01introduction.py
@@ -0,0 +1,21 @@
+# starting programming in python
+# fungsi pertama, adalah print, digunakan untuk mencetak output
+# contoh:
+
+print("Hello, World!")
+# ini akan mencetak kata Hello, World! ketika kodenya dijalankan
+# untuk memasukkan teks, kita bisa gunakan single'' atau double""
+
+# to run the code, bisa mengetikkan:
+# python <nama-file>
+
+# kita bisa membuat baris kode baru sebagai berikut:
+print("Hello, All!")
+print("I like pizza!")
+
+# comment is something like this,
+# digunakan sebagai notasi untuk anda sendiri atau orang lain untuk memahami kode,
+# juga bisa digunakan untuk menonaktifkan baris perintah python, contoh:
+
+# print("I Love Python!")
+# kode di atas tidak akan dijalankan/diabaikan oleh python interpreter
